1cbaa7f3d0eff5fad2b38b058065e42a6a14783d
Ivan <duzak2006@gmail.com>
2023-12-13 18:37:26 +0200

    Add project files.

---
+++ lab_13/lab_13.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main() {
+    int n, i;
+    unsigned long long fact = 1;
+
+    printf_s("Enter an integer: ");
+    scanf_s("%d", &n);
+
+    if (n < 0)
+        printf_s("Error! Factorial of a negative number doesn't exist.\n");
+    else {
+        for (i = 1; i <= n; ++i) {
+            fact *= i;
+        }
+        printf_s("Factorial of %d = %llu\n", n, fact);
+    }
+
+    return 0;
+}
